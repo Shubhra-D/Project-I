4e84e5e6eb22753dcf1b35a4f8f2481b7091cddd
Shubhra D <shubhradwivedi21@gmail.com>
2025-01-03 12:10:06 +0530

    got stuck in unknown problems

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,45 @@
 
 
 
+//shuttering
+document.addEventListener("DOMContentLoaded", () => {
+    const nnewLink = document.getElementById("nnew");
+    const shutter = document.querySelector(".shutter-new");
+    const closeShutter = document.getElementById("close-shutter");
+  
+    // Initially set the shutter height to 0
+    shutter.style.height = "0";
+    shutter.style.overflow = "hidden";
+    shutter.style.transition = "height 0.5s ease";
+  
+    // Function to open the shutter
+    const openShutter = () => {
+      const contentHeight = shutter.scrollHeight; // Get the full height of the shutter content
+      shutter.style.height = contentHeight + "px";
+    };
+  
+    // Function to close the shutter
+    const closeShutterEffect = () => {
+      shutter.style.height = "0";
+    };
+  
+    // Open the shutter when clicking "New"
+    nnewLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (shutter.style.height === "0px" || shutter.style.height === "") {
+        openShutter();
+      } else {
+        closeShutterEffect();
+      }
+    });
+  
+    // Close the shutter when clicking the close button
+    closeShutter.addEventListener("click", (e) => {
+      e.preventDefault();
+      closeShutterEffect();
+    });
+  });
+  
 
 
-
-
-
-
-
-
-
-
-
-
+//Modal code of user/login /sign up page
